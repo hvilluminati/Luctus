a8664832f0ba4ff4484e82d3c46f7075b93e0d24
Nina <nina.thomsen@hotmail.com>
2023-10-07 18:36:22 +0200

    Setup cards an created hand layout
    Very fine cards

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,14 +1,23 @@
+using Assets.Scripts;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Card", menuName = "Card")]
 public class Card : ScriptableObject
 {
 	public new string name;
+
+	[Multiline(4)]
 	public string description;
 
 	public Sprite artwork;
 
+	[Space]
+	public DamageType damageType;
 	public int damage;
 	public int shield;
 
+
+	[Space]
+	public CardEffect effect;
+	public int effectAmount = 0;
 }
---
+++ Assets/Scripts/CardInteraction.cs
@@ -0,0 +1,61 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts
+{
+	public class CardInteraction : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+	{
+		private RectTransform rectTransform;
+		private Canvas canvas;
+		private bool isDragging;
+		private Vector2 originalPosition;
+		private Vector2 pointerOffset;
+		public RectTransform canvasRectransform;
+
+		public void Awake()
+		{
+			rectTransform = GetComponent<RectTransform>();
+			canvas = GetComponent<Canvas>();
+		}
+
+		public void OnPointerEnter(PointerEventData eventData)
+		{
+			canvas.overrideSorting = true;
+			rectTransform.DOScale(Vector3.one, 0.3f);
+		}
+
+		public void OnPointerExit(PointerEventData eventData)
+		{
+			canvas.overrideSorting = false;
+			rectTransform.DOScale(Vector3.one * 0.75f, 0.3f);
+		}
+		public void OnPointerDown(PointerEventData eventData)
+		{
+			//Vector2 normelizedPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+			isDragging = true;
+			originalPosition = rectTransform.anchoredPosition;
+			//pointerOffset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+			RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, Input.mousePosition, eventData.pressEventCamera, out pointerOffset);
+		}
+
+		public void OnPointerUp(PointerEventData eventData)
+		{
+			isDragging = false;
+			rectTransform.DOAnchorPos(originalPosition, 0.5f);
+		}
+
+		public void Update()
+		{
+			if (isDragging)
+			{
+				Vector2 cursorPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+				//Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + pointerOffset;
+				RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRectransform, Input.mousePosition, null, out Vector2 cursorPosition);
+				rectTransform.anchoredPosition = cursorPosition - pointerOffset;
+				Debug.Log(cursorPosition);
+			}
+		}
+	}
+}
