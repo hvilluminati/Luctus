02fc7eaf38dffa3e79bf46587012426208ccdf2b
Hvilluminati <hvillum.magnus@gmail.com>
2023-10-26 18:33:30 +0200

    Merge branch 'development' of https://github.com/hvilluminati/Luctus into development

---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+	[CreateAssetMenu(fileName = "New Deck", menuName = "Deck")]
+	public class Deck : ScriptableObject
+	{
+		public List<Card> cards = new List<Card>();
+	}
+}
