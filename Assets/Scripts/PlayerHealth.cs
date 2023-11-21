d9e508d12d5dda17ddc1cc8753919359254db454
Nina <nina.thomsen@hotmail.com>
2023-11-21 12:22:38 +0100

    Small fixes

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,65 +1,65 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
 
-    public Image healthBar;
-    private float healthAmount = 100;
-    private float damageTaken;
-    public SpriteDamageEffect damageEffect;
-    public GameObject player;
+	public Image healthBar;
+	private float healthAmount = 100;
+	private float damageTaken;
+	public SpriteDamageEffect damageEffect;
+	public GameObject player;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        damageTaken = 100 - DataManager.instance.playerHealth;
-        UpdateDamage(damageTaken);
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		damageTaken = 100 - DataManager.instance.playerHealth;
+		UpdateDamage(damageTaken);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (healthAmount <= 0)
-        { SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); }
+	// Update is called once per frame
+	void Update()
+	{
+		if (healthAmount <= 0)
+		{ SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); }
 
-        if (Input.GetKeyDown(KeyCode.Backspace)) { TakeDamage(30); }
+		if (Input.GetKeyDown(KeyCode.Backspace)) { TakeDamage(30); }
 
-        if (Input.GetKeyDown(KeyCode.Return)) { HealDamage(20); }
-    }
+		if (Input.GetKeyDown(KeyCode.Return)) { HealDamage(20); }
+	}
 
-    public void UpdateDamage(float damage)
-    {
-        healthAmount -= damage;
-        healthBar.fillAmount = healthAmount / 100f;
-    }
+	public void UpdateDamage(float damage)
+	{
+		healthAmount -= damage;
+		healthBar.fillAmount = healthAmount / 100f;
+	}
 
 	public void TakeDamage(float damage)
 	{
 		healthAmount -= damage;
-        DataManager.instance.ModifyHealth(-damage);
+		DataManager.instance.ModifyHealth(-damage);
 		healthBar.fillAmount = healthAmount / 100f;
 		player.GetComponent<PlayerMovement>().Flinch();
 		damageEffect.StartFlashRenderer();
-		
+
 	}
 
 
-    public void HealDamage(float healing)
-    {
-        if((healthAmount + healing) <= 100)
-        {
-            healthAmount += healing;
-            DataManager.instance.ModifyHealth(healing);
-        }
-        else
-        {
-            healthAmount = 100;
-        }
-        
-        healthBar.fillAmount = Mathf.Clamp(healthAmount, 0, 100);
+	public void HealDamage(float healing)
+	{
+		if ((healthAmount + healing) <= 100)
+		{
+			healthAmount += healing;
+			DataManager.instance.ModifyHealth(healing);
+		}
+		else
+		{
+			healthAmount = 100;
+		}
 
-        healthBar.fillAmount = healthAmount / 100f;
-    }
+		healthBar.fillAmount = Mathf.Clamp(healthAmount, 0, 100);
+
+		healthBar.fillAmount = healthAmount / 100f;
+	}
 }
