549632abf52860cf67fb397b441d7036ed2992ba
Hvilluminati <hvillum.magnus@gmail.com>
2023-11-28 07:25:47 +0100

    Preliminarily done

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -2,19 +2,24 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-
     public AnimationCurve myCurve;
     public int distance;
+    private Vector3 initialPosition;
 
     private void Start()
     {
+        // Store the initial position of the GameObject
+        initialPosition = transform.position;
+
         if (!DataManager.instance.enemyAlive)
         {
             this.gameObject.SetActive(false);
         }
     }
+
     void Update()
     {
-        transform.position = new Vector3(myCurve.Evaluate((Time.time % myCurve.length)) + distance, transform.position.y, transform.position.z);
+        // Update the position based on the animation curve and the initial position
+        transform.position = new Vector3(initialPosition.x + myCurve.Evaluate((Time.time % myCurve.length)) * distance, initialPosition.y, initialPosition.z);
     }
 }
