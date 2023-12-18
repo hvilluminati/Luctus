eb376d8bfa42f87b098307c916b55089f7e0fcb9
Hvilluminati <hvillum.magnus@gmail.com>
2023-12-18 15:19:43 +0100

    fixed issues with enemy prefabs

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -11,7 +11,7 @@ public class PlayerHealth : MonoBehaviour
 	public SpriteDamageEffect damageEffect;
 	public GameObject player;
 	public static PlayerHealth instance;
-	private new AudioSource audio;
+	public AudioSource audio;
 
 	// Start is called before the first frame update
 	void Start()
