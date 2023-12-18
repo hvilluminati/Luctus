f10b5b43d441d30f122060e148e2bfa66cb93e36
Nina <nina.thomsen@hotmail.com>
2023-12-18 13:22:21 +0100

    All sound works. Be aware i have changed the onClick to not directly transition scene

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,8 +5,9 @@ public class EnemyMovement : MonoBehaviour
 	public AnimationCurve myCurve;
 	public int distance;
 	private Vector3 initialPosition;
-	public AudioClip soundEffect;
-	private AudioSource slimeSound;
+	//public AudioClip soundEffect;
+	[SerializeField] private AudioSource slimeSound;
+	public bool playAlways;
 
 	private void Start()
 	{
@@ -17,15 +18,13 @@ public class EnemyMovement : MonoBehaviour
 		{
 			this.gameObject.SetActive(false);
 		}
-
-		slimeSound = gameObject.AddComponent<AudioSource>();
-		slimeSound.clip = soundEffect;
+		if (playAlways) { slimeSound.Play(); }
 	}
 
 	void Update()
 	{
 		// Update the position based on the animation curve and the initial position
 		transform.position = new Vector3(initialPosition.x + myCurve.Evaluate((Time.time % myCurve.length)) * distance, initialPosition.y, initialPosition.z);
-		slimeSound.Play();
+		//slimeSound.Play();
 	}
 }
