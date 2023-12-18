0fe1ad2ced2a85ef6fe8a4c52aaa0e18193a6bc0
Hvilluminati <hvillum.magnus@gmail.com>
2023-12-18 23:48:51 +0100

    Pokemon transition music for battles, should probably be changed, but the trigger is there now

--- Assets/Scripts/EnterBattle.cs
+++ Assets/Scripts/EnterBattle.cs
@@ -7,8 +7,19 @@ public class EnterBattle : MonoBehaviour
     public Animator transition;
     public float transitionTime;
     [SerializeField] private int sceneNumber;
+    public AudioSource audio;
 
     // Update is called once per frame
+
+    private void Start()
+    {
+        // Ensure the AudioSource is disabled at the start
+        if (audio != null)
+        {
+            audio.enabled = false;
+        }
+    }
+
     public void StartBattle()
     {
         StartCoroutine(LoadLevel(sceneNumber));
@@ -18,6 +29,10 @@ public class EnterBattle : MonoBehaviour
     {
         //Play animation
         transition.SetTrigger("Battle");
+        if (audio != null)
+        {
+            audio.enabled = true;
+        }
 
         //wait
         yield return new WaitForSeconds(transitionTime);
